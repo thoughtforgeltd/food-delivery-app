bf38eac220ff930a95d0ce3613cd98e457cf5f7e
mamatagelanee <gelaneem@gmail.com>
2020-07-02 10:17:53 +0100

    [FIX] empty menu if doesn't exist on firebase

--- lib/menu/model/menus.g.dart
+++ lib/menu/model/menus.g.dart
@@ -7,8 +7,10 @@ part of 'menus.dart';
 // **************************************************************************
 
 Menus _$MenusFromJson(Map<String, dynamic> json) {
+  if (json == null) return null;
+  final meals = json['meals'] ?? Map<String, Menu>();
   return Menus(
-    meals: (json['meals'] as List)
+    meals: (meals as List)
             ?.map((e) =>
                 e == null ? null : Menu.fromJson(e as Map<String, dynamic>))
             ?.toList() ??
