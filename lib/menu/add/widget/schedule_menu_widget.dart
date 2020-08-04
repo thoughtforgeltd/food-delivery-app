c134315f622b4ec7999c3ea3faeca27fd2c48df2
mamatagelanee <gelaneem@gmail.com>
2020-08-04 13:36:30 +0100

    [ADD] used selection chip instead of cards

--- lib/menu/add/widget/schedule_menu_widget.dart
+++ lib/menu/add/widget/schedule_menu_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:fooddeliveryapp/common/widget/widget.dart';
 import 'package:fooddeliveryapp/design/colors.dart';
 import 'package:fooddeliveryapp/design/dimensions.dart';
@@ -78,15 +79,38 @@ class _ScheduleMenuWidgetState extends State<ScheduleMenuWidget> {
           scrollDirection: Axis.horizontal,
           shrinkWrap: true,
           itemBuilder: (BuildContext context, int index) {
-            return CategoryCard(
-              category: categories[index],
-              onSelected: _onCategorySelected,
-            );
+            final category = categories[index];
+            final isSelected = category?.id == state.selectedCategory;
+            return _buildCategoryChip(category, context, isSelected);
           },
         ));
   }
 
-  _onCategorySelected(Category category) {
+  Container _buildCategoryChip(
+      Category category, BuildContext context, bool isSelected) {
+    return Container(
+        margin: Dimensions.padding_4,
+        child: ChoiceChip(
+          padding: Dimensions.padding_8,
+          elevation: 4,
+          selectedColor: AppColors.colorPrimary,
+          backgroundColor: AppColors.colorWhite,
+          label: Text(category?.title,
+              style: Theme.of(context).textTheme.bodyText1.apply(
+                  color: isSelected == true ? AppColors.colorWhite : null)),
+          avatar: category.image != null
+              ? SvgPicture.network(category.image,
+                  height: 25,
+                  width: 25,
+                  fit: BoxFit.cover,
+                  color: isSelected == true ? AppColors.colorWhite : null)
+              : Icon(Icons.error, color: AppColors.colorError),
+          selected: isSelected,
+          onSelected: (selected) => _onCategorySelected(selected, category),
+        ));
+  }
+
+  _onCategorySelected(bool isSelected, Category category) {
     _mealScheduleBloc.add(CategoryChanged(category: category));
   }
 
